df3b52870d425491d40501843b045e4374ed11f2
Pedro Abreu <pedrohgabreu@inf.ufsm.br>
2025-07-08 00:24:52 -0300

    cardpost

---
+++ src/components/CardPost/index.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import { Avatar } from "../Avatar"
+
+export const CardPost = ({post}) => {
+    return (
+        <article>
+            <header>
+                <figure>
+                    <Image src={post.cover} width={438} height={133} alt={`Capa do post de titulo: ${post.title}`} />
+                </figure>
+            </header>
+            <section>
+                <h2>{post.title}</h2>
+                <h1>{post.body}</h1>
+            </section>
+            <footer>
+                <Avatar 
+                imageSrc={post.author.avatar} 
+                name={post.author.username}
+                />
+            </footer>
+        </article>
+    )
+}
